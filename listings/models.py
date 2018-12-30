bd6dcaa483baa099c749ff99a0ca5b9faf169e1b
Manoj jha <manojjha491@live.com>
2018-12-30 21:55:56 +0530

    This is after listings and listing pages are done

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
 from realtors.models import Realtors
+#from django.utils import timezone
+
 
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtors, on_delete= models.DO_NOTHING)
@@ -12,7 +14,7 @@ class Listing(models.Model):
     description = models.TextField(blank=True)
     price = models.IntegerField()
     bedroom = models.IntegerField()
-    #bathroom = models.IntegerField(max_digits=2, decimal_places=1)
+    #bathroom = models.IntegerField()
     bathroom = models.DecimalField(max_digits=2, decimal_places=1)
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
@@ -25,16 +27,7 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(default=datetime.now, blank=True )
+    list_date = models.DateTimeField(default=datetime.now(), blank=True )
 
     def __str__(self):
         return self.title
-
-
-
-
-
-
-
-
-
